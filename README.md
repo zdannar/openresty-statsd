5b955798ba933d76e446d05f35e8200ce269862b
zdannar <zack.dannar@onxmaps.com>
2018-04-06 12:10:06 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # openresty-statsd
-A simple openresty statsd implementation. 
+
+A simple(crude) openresty statsd implementation using ngx.timer.at and 
+ngxshared.DICT.  Other implimentations that I found were not thread safe and did
+not aggregate the counts.  
+
+### Disclaimer:
+
+I am not a lua guy and am very green with openresty and lua.  Use at your own risk.
+
+### nginx.conf memory allocation.
+
+```
+lua_shared_dict statsdict 1m;
+lua_shared_dict statsdumper 12k;
+```
+
+### Server endpoint config
+```
+    location /some/endpoint {
+      ...
+      ...
+        access_by_lua_block {
+            -- TODO: This could be included into a single call
+            statsd.startdumper()
+            statsd.inc()
+        }
+```
+
+
+### Statsd configuration
+
+There really isn't much going on here.  Just the ability to change the hostname and port
+
+statsd.host = "127.0.0.1"
+statsd.port = 8125 
---
+++ statsd.lua
@@ -0,0 +1,70 @@
+local _M = {}
+
+
+local function is_empty(s)
+    return s == nil or s == ""
+end
+
+_M.host = "127.0.0.1"
+_M.port = 8125
+
+local function sendit(key, cnt)
+   local sock = ngx.socket.udp()
+   local ok, err = sock:setpeername(_M.host, _M.port)
+   if not ok then
+      ngx.log(ngx.ERR, "failed to connect to statsd: ", err)
+   end
+   local suc, err = sock:send(key..":".. cnt .."|c")
+   if is_empty(suc) then
+      ngx.log(ngx.ERR,  err)
+   end
+   sock:close()
+end
+
+local function dumpkeys()
+   ngx.sleep(5)
+   keys, err = ngx.shared.statsdict:get_keys(1024)
+   if is_empty(keys) then
+      ngx.log(ngx.ERR, err)
+   else
+      for i, k in ipairs(keys) do
+         local v, _ = ngx.shared.statsdict:get(k)
+         if v > 0 then
+            sendit(k,v)
+         end
+         local n, err = ngx.shared.statsdict:incr(k, (-1*v))
+         if is_empty(n) then
+            ngx.log(ngx.ERR,  err)
+         end
+      end
+   end
+   -- This is done to make sure there isn't an integer overflow 
+   local ok, err = ngx.shared.statsdumper:set("dumper", 1)
+   if not ok then
+      ngx.log(ngx.ERR,  err)
+   end
+   ngx.timer.at(0, dumpkeys)
+end
+
+function _M.inc()
+   -- Need to split this into parts for addition to the shared dictionary
+   -- from logDNA : 'GET /arcgis/rest/services/Wisconsin/Wisconsin/MapServer/export'
+   local uri = string.lower(ngx.req.get_method()) .. string.gsub(ngx.var.request_uri, "/", ".")
+   local v, err = ngx.shared.statsdict:incr(uri, 1, 0)
+   if is_empty(v) then
+      ngx.log(ngx.ERR,  err)
+   end
+end
+
+function _M.startdumper()
+   -- This could become an issue due to integer overflow.  Revisit
+   local v, err = ngx.shared.statsdumper:incr("dumper", 1, 0)
+   if is_empty(v) then
+      ngx.log(ngx.ERR,  err)
+   end
+   if v == 1 then
+      ngx.timer.at(0, dumpkeys)
+   end
+end
+
+return _M
